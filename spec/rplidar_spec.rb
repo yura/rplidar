1ce4384547e94287d3e39e08af17f4e871ac833c
Yury Kotlyarov <yura@brainhouse.ru>
2018-08-31 22:42:24 +0700

    changing message expectations

--- spec/rplidar_spec.rb
+++ spec/rplidar_spec.rb
@@ -11,7 +11,7 @@ RESPONSE_DESCRIPTOR_SCAN = ascii("\xA5Z\x05\x00\x00@\x81")
 
 describe Rplidar do
   let(:lidar) { described_class.new('/serial') }
-  let(:port) { double('serial port') }
+  let(:port) { instance_double('serial port') }
 
   before do
     allow(Serial).to receive(:new).with('/serial', 115_200, 8, :none, 1) { port }
@@ -39,6 +39,10 @@ describe Rplidar do
   describe '#start_motor' do
     subject { lidar.start_motor }
 
+    before do
+      allow(lidar).to receive(:request_with_payload).with(0xF0, 660)
+    end
+
     it 'sends START_MOTOR command' do
       subject
       expect(lidar).to have_received(:request_with_payload).with(0xF0, 660)
@@ -47,6 +51,11 @@ describe Rplidar do
 
   describe '#stop_motor' do
     subject { lidar.stop_motor }
+
+    before do
+      allow(lidar).to receive(:request_with_payload)
+          .with(0xF0, 0).and_return(nil)
+    end
 
     it 'sends STOP_MOTOR command' do
       subject
@@ -59,7 +68,7 @@ describe Rplidar do
 
     before do
       allow(lidar).to receive(:request).with(0x20)
-      allow(port).to receive(:read).with(7) { RESPONSE_DESCRIPTOR_SCAN }
+      allow(port).to receive(:read).with(7).and_return(RESPONSE_DESCRIPTOR_SCAN)
     end
 
     it 'sends SCAN request' do
@@ -76,6 +85,10 @@ describe Rplidar do
   describe '#stop' do
     subject { lidar.stop }
 
+    before do
+      allow(lidar).to receive(:request).with(0x25)
+    end
+
     it 'sends STOP request' do
       subject
       expect(lidar).to have_received(:request).with(0x25)
@@ -87,7 +100,11 @@ describe Rplidar do
   describe '#reset' do
     subject { lidar.reset }
 
-    it 'sends STOP request' do
+    before do
+      allow(lidar).to receive(:request).with(0x40)
+    end
+
+    it 'sends RESET request' do
       subject
       expect(lidar).to have_received(:request).with(0x40)
     end
@@ -98,15 +115,24 @@ describe Rplidar do
   describe '#request' do
     subject { lidar.request(0x20) }
 
+    before do
+      allow(lidar).to receive(:port).and_return(port)
+      allow(port).to receive(:write).with(ascii("\xA5 "))
+    end
+
     it 'writes binary string to the serial port' do
       subject
-      expect(lidar).to have_received(:port).and_return(port)
+      expect(lidar).to have_received(:port)
       expect(port).to have_received(:write).with(ascii("\xA5 "))
     end
   end
 
   describe '#request_with_payload' do
     subject { lidar.request_with_payload(0xF0, 660) }
+
+    before do
+      allow(port).to receive(:write).with(ascii("\xA5\xF0\x02\x94\x02\xC1"))
+    end
 
     it 'writes binary string with payload to the serial port' do
       subject
@@ -127,12 +153,17 @@ describe Rplidar do
   describe '#close' do
     subject { lidar.close }
 
+    before do
+      allow(port).to receive(:close)
+    end
+
     it 'does not close the port if it is not open' do
       subject
       expect(port).not_to have_received(:close)
     end
 
     it 'closes the port if it is exist' do
+      # open create port first
       lidar.port
 
       subject
@@ -154,7 +185,7 @@ describe Rplidar do
 
       # call it second time
       subject
-      expect(Serial).not_to have_received(:new).with(any_args)
+      expect(Serial).to have_received(:new).with(any_args).once
     end
   end
 
@@ -172,9 +203,15 @@ describe Rplidar do
   end
 
   describe '#ints_to_binary' do
-    it 'converts integer or array of integers to the binary sequence' do
+    it 'coverts integer to the binary sequence' do
       expect(lidar.ints_to_binary(97)).to eq('a')
+    end
+
+    it 'converts array with one integer to the binary sequence' do
       expect(lidar.ints_to_binary([97])).to eq('a')
+    end
+
+    it 'converts array of integers to the binary sequence' do
       expect(lidar.ints_to_binary([0xA5, 0x20])).to eq(ascii("\xA5 "))
     end
   end
